9579b8e7f34a62c63afd6b87ab81472ec64d3076
zzq <951875842@qq.com>
2022-03-05 02:26:19 +0800

    useLayoutEffect & event

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,16 +1,45 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect, useLayoutEffect, useRef } from 'react';
 import MemoCom from './components/memo';
 import './App.css';
 
 const App = () => {
   const [name, setName] = useState({name: 'zzq'})
   const [age, setAge] = useState(24)
+  const ref = useRef(null)
+
+  const fn = (e) => {
+    if(!ref.current.contains(e.target)) {
+      console.log('xxx')
+    }
+  }
+
+  useEffect(() => {
+    document.addEventListener('click', fn)
+    ref.current.addEventListener('click', (e) => { e.stopPropagation() })
+    return document.removeEventListener('click', fn)
+  }, [])
+
+  useEffect(() => {
+    console.log('effect')
+    return (() => { console.log('destory') })
+  }, [name])
+
+  useLayoutEffect(() => {
+    console.log('layoutEffect')
+    return (() => { console.log('layoutDestory') })
+  }, [name])
+
+  console.log('render')
 
   return (
-    <div>
+    <div onClick={() => { console.log('wrap') }}>
+      {console.log('div render')}
       <button onClick={() => { setName(prev => ({name: `${prev.name}`})) }}>change name</button> name: {name.name} <br/>
       <button onClick={() => { setAge(age => age + 1) }}>change age</button>age: {age} <br/>
-      <MemoCom name={name}/>
+      <div className='test' ref={ref}>
+        <div>test</div>
+      </div>
+      {/* <MemoCom name={name}/> */}
     </div>
   )
 }
