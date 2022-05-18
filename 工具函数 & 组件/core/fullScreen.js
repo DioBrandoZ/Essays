8c526e1a2bfb6d0625bdce8b6ef1cf868ae6b491
zhaozheqi <951875842@qq.com>
2022-05-18 17:38:37 +0800

    full screen

--- 工具函数 & 组件/core/fullScreen.js
+++ 工具函数 & 组件/core/fullScreen.js
@@ -1,100 +1,47 @@
-import React, { useEffect, useState } from "react"
-
-/**
- * 
- * @param {string} videoUrl // 对局id
- * @param {string} btnClassName // 按钮样式
- * @param {object} btnStyle // 按钮行内样式
- */
-const TeachVideoBtn = ({
-  videoUrl,
-  btnClassName,
-  btnStyle,
-}) => {
-  const [isPlaying, setIsPlaying] = useState(false)
-
-  // 检测是否是全屏
-  const checkIsFullScreen = () => {
-    const isFullScreen = document.fullscreen || document.mozFullScreen || document.webkitIsFullScreen;
-    return isFullScreen === undefined ? false : isFullScreen;
-  }
-
-  // 退出全屏时暂停播放
-  const fullScreenChangeCallback = () => {
-    if (!checkIsFullScreen()) {
-      const video = document.getElementById('teach_video')
-      video.pause()
-      setIsPlaying(false)
-    }
-  }
-
-  useEffect(() => {
-    const listenerList = ['fullscreenchange', 'webkitfullscreenchange', 'mozfullscreenchange'];
-    listenerList.forEach(item => {
-      window.addEventListener(item, fullScreenChangeCallback);
-    });
-
-    return () =>
-      listenerList.forEach(item => {
-        window.removeEventListener(item, fullScreenChangeCallback);
-      });
-  }, [fullScreenChangeCallback])
-
-  // 进入全屏
-  const launchFullscreen = (element) => {
-    // 此方法不可以在異步任務中執行，否則火狐無法全屏
-    if (element.requestFullscreen) {
-      element.requestFullscreen()
-    } else if (element.mozRequestFullScreen) {
-      element.mozRequestFullScreen()
-    } else if (element.msRequestFullscreen) {
-      element.msRequestFullscreen()
-    } else if (element.oRequestFullscreen) {
-      element.oRequestFullscreen()
-    } else if (element.webkitRequestFullscreen) {
-      element.webkitRequestFullScreen()
-    }
-  }
-
-  const handleClick = () => {
-    const video = document.getElementById('teach_video')
-    video.src = videoUrl
-    launchFullscreen(video)
-    video.play()
-    setIsPlaying(true)
-  }
-
-  return <div
-    className={`${s.btn} ${btnClassName} hover-zoom-out`}
-    onClick={handleClick}
-    style={btnStyle}
-  >
-    <div className={s.free} />
-    <video
-      id="teach_video"
-      className={`${s.video} ${!isPlaying && s.hidden}`}
-      controls
-      controlsList="nodownload noplaybackrate"
-      disablePictureInPicture
-    />
-  </div>
+// 判断浏览器是否全屏
+export function getIsFullscreen() {
+  const sysFullscreen = !!(
+    document.fullscreen ||
+    document.mozFullScreen ||
+    document.webkitIsFullScreen ||
+    document.webkitFullScreen ||
+    document.msFullScreen
+  );
+  const eleFullscreen = !!(
+    document.fullscreenElement ||
+    document.msFullscreenElement ||
+    document.mozFullScreenElement ||
+    document.webkitFullscreenElement
+  );
+  return sysFullscreen || eleFullscreen || false;
 }
 
-export default TeachVideoBtn
+// 全屏
+export const requestFullScreen = (ele) => {
+  try {
+    if (ele.requestFullScreen) {
+      ele.requestFullscreen();
+    } else if (ele.mozRequestFullScreen) {
+      ele.mozRequestFullScreen();
+    } else if (ele.webkitRequestFullscreen) {
+      ele.webkitRequestFullscreen();
+    } else if (ele.msRequestFullscreen) {
+      ele.msRequestFullscreen();
+    }
+  } catch (e) {
+    console.error("全屏失败", e);
+  }
+};
 
-// .video::-webkit-media-controls-mute-button {
-//   display: none !important;
-// }
-
-
-// .video::-webkit-media-controls-current-time-display {
-//   display: none !important;
-// }
-
-// .video::-webkit-media-controls-time-remaining-display {
-//   display: none !important;
-// }
-
-// .video::-webkit-media-controls-fullscreen-button {
-//   display: none !important;
-// }
+// 退出全屏
+export const exitFullScreen = () => {
+  if (document.mozCancelFullScreen) {
+    document.mozCancelFullScreen();
+  } else if (document.webkitExitFullscreen) {
+    document.webkitExitFullscreen();
+  } else if (document.msExitFullscreen) {
+    document.msExitFullscreen();
+  } else if (document.exitFullscreen) {
+    document.exitFullscreen();
+  }
+};
