98f888cc8c044d0b864b9e9bac0061ab9d83c20a
zhaozheqi <951875842@qq.com>
2023-06-30 14:35:09 +0800

    feat: 数组不可变

---
+++ 学习笔记/Leetcode/数组不可变/index.js
@@ -0,0 +1,26 @@
+// 给定一个整数数组  nums，处理以下类型的多个查询:
+
+// 计算索引 left 和 right （包含 left 和 right）之间的 nums 元素的 和 ，其中 left <= right
+// 实现 NumArray 类：
+
+// NumArray(int[] nums) 使用数组 nums 初始化对象
+// int sumRange(int i, int j) 返回数组 nums 中索引 left 和 right 之间的元素的 总和 ，包含 left 和 right 两点（也就是 nums[left] + nums[left + 1] + ... + nums[right] )
+
+/**
+ * @param {number[]} nums
+ */
+var NumArray = function(nums) {
+  this.arr = new Array(nums.length + 1).fill(0)
+  nums.forEach((n, i) => {
+    this.arr[i + 1] = n + this.arr[i]
+  })
+};
+
+/** 
+ * @param {number} left 
+ * @param {number} right
+ * @return {number}
+ */
+NumArray.prototype.sumRange = function(left, right) {
+  return this.arr[right + 1] - this.arr[left]
+};
