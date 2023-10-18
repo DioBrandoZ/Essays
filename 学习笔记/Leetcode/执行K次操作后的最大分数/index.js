8b500b73e2c843fd695ff25bd0215b1be922810a
zhaozheqi <951875842@qq.com>
2023-10-18 11:25:28 +0800

    leetcode: 最大分数

---
+++ 学习笔记/Leetcode/执行K次操作后的最大分数/index.js
@@ -0,0 +1,18 @@
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number}
+ */
+var maxKelements = function(nums, k) {
+  q = new MaxPriorityQueue();
+  let ans = 0;
+  for (const num of nums) {
+      q.enqueue(num);
+  }
+  for (let i = 0; i < k; i++) {
+      const x = q.dequeue().element;
+      ans += x;
+      q.enqueue(Math.ceil(x / 3));
+  }
+  return ans;
+};
